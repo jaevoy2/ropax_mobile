f5b25cef4e0ed77b01de1296a78fc420c7f93edd
Jae Voy Seloterio <sjaevoy@gmail.com>
2026-01-19 00:23:36 +0800

    revisions-almost-done

--- .history/utils/channel_20251210215621.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "./supabase";
-
-export const channel = supabase.channel('seat_selection')
-    .on('postgres_changes', { event: '*', schema: 'public', table: 'seats_channels' }, (payload) => {
-        console.log('Seat selected:', payload);
-
-    })
-    .subscribe();
