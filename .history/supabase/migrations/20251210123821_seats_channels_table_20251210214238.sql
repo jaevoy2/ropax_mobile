d99043efb3d8eb0bfafc73dc44d20594027ac97d
Jae Voy Seloterio <sjaevoy@gmail.com>
2026-02-17 00:44:27 +0800

    Untrack env and history

--- .history/supabase/migrations/20251210123821_seats_channels_table_20251210214238.sql
+++
@@ -1,9 +0,0 @@
-create table seats_channels (
-    id bigint generated always as identity primary key,
-    trip_id bigint not null,
-    seat_number varchar not null,
-    device_id null,
-    created_at timestamp default now()
-);
-
-create unique index unique_seat_device on seats_channels (seat_number, device_id);
--- .history/utils/supabase_20251209224027.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.PUBLIC_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
